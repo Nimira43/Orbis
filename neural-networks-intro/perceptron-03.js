f3f7307ae3081fffda72ef1932088c79ac8d7cdb
Nimira43 <n.rabone72@aol.com>
2025-11-14 00:28:38 +0000

    Archiving perceptron-02.

---
+++ neural-networks-intro/perceptron-03.js
@@ -0,0 +1,74 @@
+const trainInputs = [
+  [2, 7],
+  [3, 6],
+  [1, 1],
+  [1, 2],
+  [2, 1]
+]
+
+const trainLabels = [1, 1, 0, 0, 0]
+
+class Perceptron {
+  constructor(learningRate = 0.1) {
+    this.weights = [0.1, -0.3]
+    this.bias = 0.5
+    this.learningRate = learningRate
+  }
+
+  activationFunction(x) {
+    return (
+      x >= 0
+        ? 1
+        : 0
+    )
+  }
+  
+  predict(inputs) {
+    let sum = this.bias
+    for (let j = 0; j < inputs.length; j++) {
+      sum += inputs[j] * this.weights[j]
+    }
+    return this.activationFunction(sum)
+  }
+
+  train(trainData, trainLabels) {
+    for (let i = 0; i < trainData.length; i++) {
+      let inputs = trainData[i]
+      const yPredicted = this.predict(inputs)
+      const yTrueValue = trainLabels[i]
+     
+      if (yTrueValue != yPredicted) {
+        for (let k = 0; k < this.weights.length; k++) {
+          this.weights[k] += this.learningRate * (yTrueValue - yPredicted) * inputs[k] 
+        }
+
+        this.bias += this.learningRate * (yTrueValue - yPredicted)
+      }
+    }
+  }
+
+  calculateAccuracy(inputs, labels) {
+    let correct = 0
+
+    for (let i = 0; i < inputs.length; i++) {
+      const yPredicted = this.predict(inputs[i])
+
+      if (yPredicted === labels[i]) {
+        correct++
+      }
+    }
+    return (correct / inputs.length) * 100
+  }
+}
+
+const perceptron = new Perceptron()
+const epochs = 10
+
+for (let epoch = 0; epoch < epochs; epoch++) {
+  perceptron.train(trainInputs, trainLabels)
+  console.log('Epoch: ', epoch)
+  console.log(perceptron)
+}
+
+const trainingAccuracy = perceptron.calculateAccuracy(trainInputs, trainLabels)
+console.log(`TRAINING ACCURACY: ${trainingAccuracy}%`)
