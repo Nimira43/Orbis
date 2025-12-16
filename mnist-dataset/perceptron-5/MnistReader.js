b3acab9abe3e21a106608bbad31979195637e04f
Nimira43 <n.rabone72@aol.com>
2025-12-16 22:46:32 +0000

    Storing number of images, height and pixel of images.

--- mnist-dataset/perceptron-5/MnistReader.js
+++ mnist-dataset/perceptron-5/MnistReader.js
@@ -10,8 +10,19 @@ function readIdxFile(filePath) {
   let offset = 0
 
   // first 4 bytes = magicNumber (identifies IDX file type)
-  const magicNumber = data.readUInt32BE()
-  console.log(magicNumber)
+  const magicNumber = data.readUint32BE()
+  offset += 4
+
+  const numberOfItems = data.readUint32BE(offset)
+  // total images stored in this IDX file
+  offset += 4
+
+  const row = data.readUint32BE(offset)
+  // image height in pixels
+  offset += 4
+
+  const cols = data.readUint32BE(offset)
+  // image width in pixels
 }
 
 readIdxFile('./datasets/mnist/train-images.idx3-ubyte')
