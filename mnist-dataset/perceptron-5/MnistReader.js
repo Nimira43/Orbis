9f15f698962d8e2be420cda518f268e140fab5b2
Nimira43 <n.rabone72@aol.com>
2025-12-16 22:35:41 +0000

    Reading IDX file. Commented code.

--- mnist-dataset/perceptron-5/MnistReader.js
+++ mnist-dataset/perceptron-5/MnistReader.js
@@ -1,7 +1,17 @@
 const fs = require('fs')
 
+// readIdxFile: loads raw IDX binary file into a Buffer
 function readIdxFile(filePath) {
+  
+  // read entire IDX file into memory
+  const data = fs.readFileSync(filePath)
+  
+  // pointer for stepping through the binary structure
+  let offset = 0
 
+  // first 4 bytes = magicNumber (identifies IDX file type)
+  const magicNumber = data.readUInt32BE()
+  console.log(magicNumber)
 }
 
 readIdxFile('./datasets/mnist/train-images.idx3-ubyte')
