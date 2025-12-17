229120d87c7a64c74ba58241f226634923ed6ce2
Nimira43 <n.rabone72@aol.com>
2025-12-17 10:55:31 +0000

    Router - 2

--- frontend/src/routes/router.js
+++ frontend/src/routes/router.js
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
+import HomePage from '../pages/Home.jsx'
+import PreviewTestImages from '../pages/PreviewTestImages.jsx'
+
 
 function Router() {
   return (
     <main className='page-root'>
       <Routes>
-        <Route
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/test-images' element={<PreviewTestImages />}/>
       </Routes>
     </main>
   )
