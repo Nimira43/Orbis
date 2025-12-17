84c741dc6a8524e90a64308448076e4f09514ba3
Nimira43 <n.rabone72@aol.com>
2025-12-17 10:09:33 +0000

    Router - 1

--- frontend/src/routes/router.js
+++ frontend/src/routes/router.js
@@ -1,3 +1,14 @@
+import { useState } from 'react'
+import PreviewTestImagesPage from '../pages/mnist/PreviewTestImages'
+import HomePage from '../pages/Home'
+
 export function Router() {
-  
+  const [pathname, setPathname] = useState(window.location.pathname)
+
+  switch (pathname) {
+    case '/test-images':
+      return <PreviewTestImagesPage />
+    default:
+      return <HomePage />
+  }
 }
