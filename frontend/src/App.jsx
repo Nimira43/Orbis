817c2ff189909d43a9e4b78242b9cc14933c8213
Nimira43 <n.rabone72@aol.com>
2025-12-17 10:49:35 +0000

    Imported Router into App.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import Navigation from './components/Navigation'
+import Router from './routes/router'
 
 function App() {
   return (
     <>  
       <Navigation />
+      <Router />
     </>
   )
 }
