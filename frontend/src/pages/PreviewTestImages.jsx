330e2d6fd68290c8e8acf8e5e7ebdb2f873a74d8
Nimira43 <n.rabone72@aol.com>
2026-01-30 21:04:17 +0000

    Creating image from data.

--- frontend/src/pages/PreviewTestImages.jsx
+++ frontend/src/pages/PreviewTestImages.jsx
@@ -9,11 +9,35 @@ function PreviewTestImagesPage() {
       .then(data => setMnistData(data))
   }, [])
 
+  const createImageUrl = (inputs) => {
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+    canvas.width = 28
+    canvas.height = 28
+
+    const imageData = ctx.createImageData(28, 28)
+
+    for (let i = 0; i < imageData.height; i++) {
+      for (let j = 0; j < imageData.width; j++) {
+        const pixelValue = inputs[i][j]
+        const idx = (i * imageData.width + j) * 4
+        imageData.data[idx] = pixelValue
+        imageData.data[idx + 1] = pixelValue 
+        imageData.data[idx + 2] = pixelValue
+        imageData.data[idx + 3] = 255
+      }
+    }
+    ctx.putImageData(imageData, 0, 0)
+    return canvas.toDataURL()
+  }
+
   if (!mnistData) {
     return (
       <div>Loading...</div>
     )
   }
+
+  const { inputs, labels } = mnistData
 
   return (
     <>
@@ -22,8 +46,20 @@ function PreviewTestImagesPage() {
           Mnist Test Images
         </div>
         <div className='page-content'>
-          <div className='image'>
-            
+          <div className='images'>
+            {inputs.map((input, index) => (
+              <div
+                key={index}
+                className='image-container'
+              >
+                <img
+                  alt={`Digit ${labels[index]}`}
+                  src={createImageUrl(input)}
+                />
+                <p>
+                  Label: {labels[index]}, Index: {index}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
