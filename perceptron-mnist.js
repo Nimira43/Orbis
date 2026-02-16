041bbf219bd969acb5fc5a2a19a6e94d28dc5ca4
Nimira43 <n.rabone72@aol.com>
2026-02-16 22:48:35 +0000

    Training Perceptron with training data set

--- perceptron-mnist.js
+++ perceptron-mnist.js
@@ -1,35 +1,20 @@
+const fs = require('fs')
 const seedrandom = require('seedrandom')
 const seed = 'perc-1'
 
 seedrandom(seed, {global: true})
 
-const trainInputs = [
-  [2, 7],
-  [3, 6],
-  [1, 1],
-  [1, 2],
-  [2, 1]
-]
-
-const testInputs = [
-  [2, 6],
-  [3, 7],
-  [1, 3],
-  [2, 2],
-  [2, 5],
-]
-
-// Labels are the numbers 0 to 9
-const trainLabels = [1, 1, 0, 0, 0]
-const testLabels = [1, 1, 0, 0, 1]
-
 class Perceptron {
-  constructor(learningRate = 0.1) {
-    this.weights = Array(2).fill(0).map(() => 
-      Math.random() * 0.5 - 0.2
-    )
-    this.bias = Math.random() * 0.5 - 0.2
-    console.log('WEIGHTS: ', this.weights) 
+  constructor(
+    inputSize,
+    learningRate = 0.1
+  ) {
+    this.weights = Array(inputSize)
+      .fill(0)
+      .map(() => 
+        Math.random() * 0.5 - 0.2
+      )
+    this.bias = Math.random() * 0.3 - 0.1 
     this.learningRate = learningRate
   }
 
@@ -72,20 +57,28 @@ class Perceptron {
   }
 }
 
-const perceptron = new Perceptron(0.07)
-const epochs = 9
+const epochs = 100
+const trainBatches = 10
+const INPUT_SIZE = 28 * 28
+const trainInputs = []
+const trainLabels = []
+
+for (let i = 0; i < trainBatches; i++) {
+  const { inputs, labels } = JSON.parse(fs.readFileSync(`./datasets/mnist/train-data-${i}.json`, 'utf8'))
+  trainInputs.push(...inputs)
+  trainLabels.push(...labels)
+}
+
+const perceptron = new Perceptron(INPUT_SIZE, 0.01)
 
 for (let epoch = 0; epoch < epochs; epoch++) {
   perceptron.train(trainInputs, trainLabels)
 
   const trainingAccuracy = perceptron.calculateAccuracy(trainInputs, trainLabels)
-  const testingAccuracy = perceptron.calculateAccuracy(testInputs, testLabels)
+  // const testingAccuracy = perceptron.calculateAccuracy(testInputs, testLabels)
 
   console.log(`EPOCH: ${epoch + 1}`)
   console.log(`Training Accuracy: ${trainingAccuracy}%`)
-  console.log(`Testing Accuracy: ${testingAccuracy}%`)
+  // console.log(`Testing Accuracy: ${testingAccuracy}%`)
   console.log('---------------------------')
 }
-
-
-
